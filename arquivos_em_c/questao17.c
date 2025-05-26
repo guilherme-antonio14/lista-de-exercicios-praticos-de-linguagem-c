a6acf2cbab6ceb6730948684517e2dfcfc19c191
Guilherme Antônio <guiantoniosf@gmail.com>
2025-05-26 20:21:06 -0300

    adicionei uma function multiplicar

--- arquivos_em_c/questao17.c
+++ arquivos_em_c/questao17.c
@@ -7,13 +7,19 @@ Utilize pelo menos 1.000.000 de termos para obter uma precisão de pelo menos 6 
 #include <stdio.h>
 #include <math.h>
 
+float multiplicar(int limit, float pil){
+        for(int n = 0; n < limit; n++) {
+        pil += pow(-1, n) / (2 * n + 1);
+    }
+    return pil;
+}
+
 int main() {
     int limites = 1000000; // Limite de números a serem calculados
     float pi = 0.0;
+    
+    pi = multiplicar(limites, pi);
 
-    for (int n = 0; n < limites; n++) {
-        pi += pow(-1, n) / (2 * n + 1);
-    }
     printf("pi = %.6f\n", pi * 4); // %.6Lf para imprimir 6 casas decimais
     return 0;
 }
