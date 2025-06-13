4203fd80a2b7af78457f2aee27126db0deaa3fcf
Guilherme Antônio <guiantoniosf@gmail.com>
2025-06-13 11:00:24 -0300

    adionei um contador na q12

--- arquivos_em_c/lista3/q12.c
+++ arquivos_em_c/lista3/q12.c
@@ -8,19 +8,22 @@ int main(){
     int horario = time(NULL);
     srand(horario);
     int n = rand() % 101;
-    int i;
+    int i, e = 0;
     do{
         printf("Adivinhe o numero:\n");
         scanf("%i", &i);
         if(i > n){
             printf("Seu número é maior que o número sorteado!!\n");
+            e++;
         } 
         else if(i < n){
             printf("Seu número é menor que o número sorteado!!\n");
+            e++;
         }
         else{
-            printf("Acertou!! %d\n", i);
+            printf("Acertou!! %d\nTentativas:\t%d", i, e);
         }
     }while(i != n);
+    printf("\n");
     return 0;
 }
